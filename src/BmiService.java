4e01f1e50e2ea97b05bd7080f896f6a7c6ec94d8
Sillis Kan <it.kan@bk.ru>
2023-06-04 16:36:13 +0500

    load Testability

---
+++ src/BmiService.java
@@ -0,0 +1,5 @@
+public class BmiService {
+    int calculate (double height,int mass) {
+        return (int) (mass / Math.pow(height, 2) );
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Задание 1
+
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
+
+        // Задание 2
+
+        BmiService bmiService = new BmiService();
+
+        int bmi = bmiService.calculate(1.87, 98);
+        System.out.println(bmi);
+
+        // Задание 3
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите сумму займа: ");
+        double credit = Double.parseDouble(scan.nextLine());
+        System.out.println("сумма займа = " + credit);
+
+        System.out.println("Введите процентную ставку: ");
+        double percent = Double.parseDouble(scan.nextLine());
+        System.out.println("процентная ставка = " + percent);
+
+        System.out.println("Введите годовой период погашения: ");
+        int period = Integer.parseInt(scan.nextLine());
+        System.out.println("Период погашения = " + period);
+
+        double payments = new CreditPaymentService().calculate(credit, percent, period);
+
+        System.out.println("Ваш ежемесечный платёж состовляет = " + payments);
+
     }
 }
